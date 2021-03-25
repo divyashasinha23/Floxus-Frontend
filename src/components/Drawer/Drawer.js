8a231c23c7e861bb76dcfb0d5bdf15ebcaf52a73
nagendra0032 <nagendrakumar0032@gmail.com>
2021-03-25 13:55:11 +0530

    mobile nav design enhanced

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -11,13 +11,96 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import * as CgIcons from 'react-icons/cg';
-import Button1 from '../Buttons/Button1';
-import Buttoncomps from '../Buttons/Button1';
 import * as IoIcons from 'react-icons/io';
+import styled from '@emotion/styled';
+import CancelTwoToneIcon from '@material-ui/icons/CancelTwoTone';
+import KeyboardArrowRightTwoToneIcon from '@material-ui/icons/KeyboardArrowRightTwoTone';
+import { Link } from 'react-router-dom';
+const Header = styled.div`
+  display: flex;
+  width: 100%;
+  ${'' /* background-color: black; */}
+  align-items: center;
+  height: 90px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+const ItemContainer = styled.div`
+  display: flex;
+  width: 100%;
+  ${'' /* background-color: black; */}
+  align-items: center;
+  justify-content: center;
+  height: 60px;
+  border-top: 1px solid #cecce3;
+
+  .link {
+    display: flex;
+    height: 30px;
+    width: 100%;
+    text-decoration: none;
+    color: #212121;
+  }
+
+  .content {
+    display: flex;
+    height: 30px;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    ${'' /* background-color: black; */}
+  }
+
+  p {
+    font-size: 17px;
+    font-weight: 600;
+    margin-left: 30px;
+  }
+`;
+
+const HorizontalLine = styled.div`
+  width: 100%;
+  height: 0.8px;
+  background-color: #cecce3;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: auto;
+  justify-content: center;
+  align-items: center;
+
+  .link {
+    display: flex;
+    width: 100%;
+    height: auto;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    color: white;
+  }
+`;
+
+const JoinUsButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  border: none;
+  width: 80%;
+  height: 50px;
+  border-radius: 25px;
+  margin-top: 30px;
+  background: transparent linear-gradient(85deg, #ed841f 0%, #ff5400 100%) 0% 0%
+    no-repeat padding-box;
+  font-size: 17px;
+  font-weight: 600;
+`;
 
 const useStyles = makeStyles({
   list: {
-    width: 250,
+    width: '100vw',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -58,7 +141,7 @@ export default function Drawerright() {
         //   flexDirection: 'column',
         // }}
       >
-        <IoIcons.IoMdCloseCircle
+        {/* <IoIcons.IoMdCloseCircle
           style={{
             fontSize: '30px',
             color: 'purple',
@@ -75,8 +158,44 @@ export default function Drawerright() {
               <ListItemText primary={text} />
             </ListItem>
           ))}
-        </List>
-        {/* <Buttoncomps value="Sigsnup" onClick={toggleDrawer(anchor, false)} /> */}
+        </List> */}
+        <Header>
+          <CancelTwoToneIcon style={{ color: 'blue', fontSize: '30px' }} />
+        </Header>
+        <ItemContainer>
+          <a href="/#course" className="link">
+            <div className="content">
+              <p>Courses</p>
+              <KeyboardArrowRightTwoToneIcon
+                style={{ color: '#FF5400', marginRight: '40px' }}
+              />
+            </div>
+          </a>
+        </ItemContainer>
+        <ItemContainer>
+          <Link to="#" className="link">
+            <div className="content">
+              <p>Contact Us</p>
+              <KeyboardArrowRightTwoToneIcon
+                style={{ color: '#FF5400', marginRight: '40px' }}
+              />
+            </div>
+          </Link>
+        </ItemContainer>
+        <ItemContainer>
+          <div className="content">
+            <p>Become a Mentor</p>
+            <KeyboardArrowRightTwoToneIcon
+              style={{ color: '#FF5400', marginRight: '40px' }}
+            />
+          </div>
+        </ItemContainer>
+        <HorizontalLine />
+        <ButtonContainer>
+          <Link to="/join-us" className="link">
+            <JoinUsButton>Join Us</JoinUsButton>
+          </Link>
+        </ButtonContainer>
       </div>
     </>
   );
